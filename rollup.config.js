a3bb942907a7ebf4d56ccb0a23a1d44d90aa6f9c
Matt Bomhoff <matthew.bomhoff@gmail.com>
2023-08-30 01:15:43 -0700

    Cleanup

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import css from "rollup-plugin-import-css";
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
+
+export default {
+    input: 'src/index.js',
+    plugins: [
+        nodeResolve({
+            extensions: ['.js', 'jsx']
+        }),
+        babel({
+            babelHelpers: 'bundled',
+            presets: ['@babel/preset-react'],
+            extensions: ['.js']
+        }),
+        commonjs(),
+        replace({
+            preventAssignment: false,
+            'process.env.NODE_ENV': '"development"'
+        }),
+        css()
+    ],
+    output: [
+        { 
+            file: 'dist/index.js',
+            format: 'cjs'
+        },         
+    ],
+};
